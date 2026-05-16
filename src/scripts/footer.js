a1c3ab941f00c4249e8f3aeb2ecf4d878038fdd8
Kaden <kadenissilly.social@gmail.com>
2026-05-16 15:45:47 +0100

    Fixed script

--- src/scripts/footer.js
+++ src/scripts/footer.js
@@ -1,11 +1,11 @@
-document.getElementById('year').textContent = new Date().getFullYear();
+const commitSHA = document.getElementById('commit-sha');
 
 try {
     const github = await fetch('https://api.github.com/repos/KadenIsSilly/kaden-the.dev/commits?per_page=1');
     const data = await github.json();
-    const commitSHA = document.getElementById('commit-sha');
+    console.log(data);
 
-    commitSHA.href = data[0].url;
+    commitSHA.href = data[0].html_url;
     commitSHA.textContent = ` (${data[0].sha.slice(0, 7)})`;
 } catch {
     commitSHA.textContent = '';
